3109c7fc9db7d331854646fd0f91c972251982bb
benmatheson <mathesonben@gmail.com>
2021-01-31 11:44:47 -0900

    labels

--- js/main.js
+++ js/main.js
@@ -19,13 +19,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-
-
-<TextSummary data={this.state} />
+        <TextSummary data={this.state} />
 
         <PfdBanner data={this.state} />
-        <PfdBannerLabel  />
-       
+        <PfdBannerLabel />
 
         <AmountSlider handler={this.handleChange} />
 
@@ -64,36 +61,18 @@ class AmountSlider extends React.Component {
 class PfdBanner extends React.Component {
   render() {
     return (
-  
       <div className="ban">
         ${parseInt(this.props.data.pfd).toLocaleString()}
       </div>
-          
-  
     );
   }
 }
 
-
-
 class PfdBannerLabel extends React.Component {
-    render() {
-      return (
-    
-        <div className="lan">
-        PFD Size
-        </div>
-            
-    
-      );
-    }
+  render() {
+    return <div className="lan">PFD Size</div>;
   }
-  
-
-
-
-
-
+}
 
 class TextSummary extends React.Component {
   // componentDidMount () {
@@ -129,7 +108,7 @@ class TextSummary extends React.Component {
   render() {
     // console.log("RENDINGRS TEXd")
     const pfd = this.props.data.pfd;
-    const total_cost_of_pfds = pfd * 639247 ; //this is the 2018 total. 
+    const total_cost_of_pfds = pfd * 639247; //this is the 2018 total.
     // console.log(total_cost_of_pfds)
     const annual_return = 0.07;
     const annual_return_full = 1.07;
@@ -137,14 +116,6 @@ class TextSummary extends React.Component {
     const annual_earnings = Math.floor(total_cost_of_pfds * annual_return);
 
     // console.log(total_cost_of_pfds*(1+annual_return)^3)
-
-
-
-
-
-
-
-
 
     const lost_earnings_array = [];
 
@@ -154,76 +125,114 @@ class TextSummary extends React.Component {
         pfd: pfd,
         year: i,
         money: total_cost_of_pfds * (1 + annual_return) ** i, //the compounded spend total
-        annual_earnings_lost: annual_earnings * (1 + annual_return) ** (i-1), // the compouned  earnings. hm
+        annual_earnings_lost: annual_earnings * (1 + annual_return) ** (i - 1), // the compouned  earnings. hm
         // annual_earnings_lost_1: total_cost_of_pfds * (1+annual_return) ** (i), // the compouned  earnings. hm
       };
 
       lost_earnings_array.push(yearMoney);
     }
 
-    console.log(lost_earnings_array)
+    console.log(lost_earnings_array);
+
+    const total_lost_earnings = Math.floor(
+      lost_earnings_array.reduce(
+        (total, curr) => (total += curr.annual_earnings_lost),
+        0
+      )
+    );
+
+    console.log(total_lost_earnings);
+
+    // #########FoRMATTING
+
+    var total_cost_of_pfds_format;
+
+    total_cost_of_pfds < 1000000000
+      ? (total_cost_of_pfds_format = `$${Math.floor(
+          total_cost_of_pfds / 1000000
+        )} million`)
+      : (total_cost_of_pfds_format = `$${(
+          total_cost_of_pfds / 1000000000
+        ).toFixed(2)} billion`);
+    console.log(total_cost_of_pfds_format);
+
+    var annual_earnings_format;
+    annual_earnings < 1000000000
+      ? (annual_earnings_format = `$${Math.floor(
+          annual_earnings / 1000000
+        )} million`)
+      : (annual_earnings_format = `$${(annual_earnings / 1000000000).toFixed(
+          2
+        )} billion`);
+
+    var total_lost_earnings_format;
+
+    total_lost_earnings < 1000000000
+      ? (total_lost_earnings_format = `$${Math.floor(
+          total_lost_earnings / 1000000
+        )} million`)
+      : (total_lost_earnings_format = `$${(
+          total_lost_earnings / 1000000000
+        ).toFixed(2)} billion`);
+
+    // #########FoRMATTING
+
+    // ###################
+
+    const width = window.innerWidth;
+
+    var spacing; // this is the i * spacing
+    var barWidth;
+
+    width >= 767
+      ? (spacing = (width * 0.55) / 20)
+      : (spacing = (width - 30) / 20);
+    width >= 767
+      ? (barWidth = (width * 0.4) / 20)
+      : (barWidth = (width - 50) / 20);
+
+    const scaleY = d3.scaleLinear();
+    scaleY.range([0, 85]).domain([0, 1400000000]);
+    // 9 639 041 650.718876
+    // console.log(scaleX(2000000000))
+
+    // #####################
+
+    // var max_visits = Math.max(...busy_data_current.map(d=> d.tests))
+    var y_axis = d3.axisLeft(scaleY);
+    var ticks = y_axis.ticks();
+    var ticks_array = scaleY.ticks(4);
+
+console.log("TICKS")
+console.log(ticks)
+console.log(ticks_array)
+
+    {
+      /* <text x= "20" y= "10"  className = "busy_text bold" >Typical Tests Per Hour</text> */
+    }
+    {
+      /* <g id={`_${this.props.data.location}`} ref = {this.svg_ref}>   </g> */
+    }
+    // <g ref = {this.svg_ref}  >
+
+    var p = d3.precisionPrefix(1e10, 1.3e6),
+    formater = d3.formatPrefix("." + p, 1.3e6);
 
 
-const total_lost_earnings = Math.floor(lost_earnings_array.reduce(( total, curr)=>total+=curr.annual_earnings_lost,0))
+//    var formater = d3.formatPrefix("." + d, 1.3e6)
 
 
-console.log(total_lost_earnings)
-
-
-
-
-// #########FoRMATTING
-
-
-var total_cost_of_pfds_format;
-
-total_cost_of_pfds <1000000000 ? total_cost_of_pfds_format = `$${Math.floor(total_cost_of_pfds/1000000)} million` : total_cost_of_pfds_format = `$${(total_cost_of_pfds/1000000000).toFixed(2)} billion`
- console.log(total_cost_of_pfds_format)
-
-
- var annual_earnings_format;
- annual_earnings <1000000000 ? annual_earnings_format = `$${Math.floor(annual_earnings/1000000)} million` : annual_earnings_format = `$${(annual_earnings/1000000000).toFixed(2)} billion`
-
- var total_lost_earnings_format;
-
- total_lost_earnings <1000000000 ? total_lost_earnings_format = `$${Math.floor(total_lost_earnings/1000000)} million` : total_lost_earnings_format = `$${(total_lost_earnings/1000000000).toFixed(2)} billion`
-
-
-
-
-// #########FoRMATTING
-
-
-
-// ###################
-
-
-const width = window.innerWidth;
-
-
-
-var spacing; // this is the i * spacing
-var barWidth;
-
-width >=767 ? spacing = (width *.55)/20 : spacing  =  (width - 30) /20
-width >=767 ? barWidth = (width *.4)/20 : barWidth  =  (width - 50) /20
-
-const scaleY = d3.scaleLinear()
-scaleY.range([0,85]).domain([0, 1400000000])
-// 9 639 041 650.718876
-// console.log(scaleX(2000000000))
-
-
-
-// #####################
-
-
+    {
+      /* ###################### */
+    }
 
     return (
       <div>
-        <p className ="TextSummary">
+        <p className="TextSummary">
           An Alaska PFD of{" "}
-          <span className="highlight green">${parseInt(pfd).toLocaleString()} </span>
+          <span className="highlight green">
+            ${parseInt(pfd).toLocaleString()}{" "}
+          </span>
           requires spending{" "}
           <span className="highlight red">
             {/* ${total_cost_of_pfds.toLocaleString()}{" "} */}
@@ -231,23 +240,37 @@ scaleY.range([0,85]).domain([0, 1400000000])
           </span>
           from savings accounts like the Alaska Permanent Fund. <br /> <br />
           Due to the lost earning power of those assets, that costs Alaskans{" "}
-          <span className="highlight red">
-            {annual_earnings_format}
-          </span>{" "}
-          in missed growth in 2021. Compounded over 20 years, this would cost           <span className="highlight red">
-{total_lost_earnings_format}</span>
+          <span className="highlight red">{annual_earnings_format}</span> in
+          missed growth in 2021. Compounded over 20 years, this would cost{" "}
+          <span className="highlight red">{total_lost_earnings_format}</span>
         </p>
 
+        <svg width={width - 30} height="90">
+          {lost_earnings_array.map(function (d, i) {
+            return (
+              <rect
+                className="rect"
+                x={i * spacing}
+                y={5}
+                width={barWidth}
+                height={scaleY(d.annual_earnings_lost)}
+              ></rect>
+            );
+          })}
 
-        <svg width= {width-30} height ="90">
-        {lost_earnings_array.map(function(d,i){
-            return (<rect className="rect" x={i*spacing} y={5} width={barWidth} height ={scaleY(d.annual_earnings_lost)} ></rect>)
+          <g>
 
+          {ticks_array.map(function (d, i) {
+      return (
+        <text class="bold" fontSize={8} x={3} y={ scaleY(d)+10}>
+          {/* {formater(d)} */}
+          {`$${d/1000000} million`}
+        </text>
+      );
+    })}
 
-        })}
-
+          </g>
         </svg>
-
       </div>
     );
   }
